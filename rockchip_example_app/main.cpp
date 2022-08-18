40619c32c4a5cdd761e57329c35e1965f324e823
Umesh <umesh.emmadiit@gmail.com>
2022-08-18 17:48:45 -0400

    Work in Progress Commit -Added debug libraries -work around the spi lib and keeping it simple to work and control the led ring

--- rockchip_example_app/main.cpp
+++ rockchip_example_app/main.cpp
@@ -1,4 +1,6 @@
 #include "LEDHandler.hpp"
+#include "inspcore.hpp"
+
 
 using namespace LEDHANDLER;
 
@@ -7,7 +9,11 @@ LEDHandler* pLEDHandler;
 
 int main()
 {
-	pLEDHandler = new LEDHandler();
+
+    InspCore::setFormattedDebugOutput("Led App");
+    qDebug()<<__LINE__;
+#if 1
+    pLEDHandler = new LEDHandler();
 
     // spi dev filename const is in ../src/internal/TwoWireSpiImple.h
     // spi linux handler is in ../src/internal/SPIHandler.h
@@ -15,6 +21,7 @@ int main()
     pLEDHandler->startSpi();
 
     pLEDHandler->turnOffAllLEDs();
+    pLEDHandler->testLights();
 
     // test states of some leds
     pLEDHandler->setLEDState(eLED_STATE_LASER_DOT_ON);
@@ -25,6 +32,51 @@ int main()
 
     // test battery led blinking
     pLEDHandler->setLEDBatteryStatusState(eLED_BATTERY_CHARGING);
+    pLEDHandler->testLights();
+    
+#endif
+
+#if 0
+    const uint16_t pixels = 22;
+    const RgbColor COLOR_RED        (255,   0,   0);
+    const RgbColor COLOR_BLUE       (  0,   0, 255);
+    const RgbColor COLOR_GREEN      (  0, 255,   0);
+    const RgbColor COLOR_LIGHT_GREEN ( 19, 255,  15);
+    const RgbColor COLOR_WHITE      (255, 255, 255);
+    const RgbColor COLOR_BLACK      (  0,   0,   0);
+    const RgbColor COLOR_YELLOW     (255, 255,   0);
+    const RgbColor COLOR_PURPLE     (255,   0, 255);
+    const RgbColor COLOR_ORANGE     (255, 50,   0);
+
+    NeoPixelBus<DotStarBgrFeature, DotStarSpi2MhzMethod> mTrial(pixels);
+    mTrial.Begin();
+    mTrial.ClearTo(COLOR_BLACK);
+    mTrial.Show();
+
+    mTrial.SetPixelColor(1, COLOR_RED);
+    mTrial.SetPixelColor(2, COLOR_GREEN);
+    mTrial.SetPixelColor(3, COLOR_ORANGE);
+    mTrial.SetPixelColor(4, COLOR_BLUE);
+    mTrial.SetPixelColor(5, COLOR_GREEN);
+    mTrial.SetPixelColor(6, COLOR_ORANGE);
+    mTrial.SetPixelColor(7, COLOR_RED);
+    mTrial.SetPixelColor(8, COLOR_BLUE);
+    mTrial.SetPixelColor(9, COLOR_GREEN);
+    mTrial.SetPixelColor(10, COLOR_ORANGE);
+    mTrial.SetPixelColor(11, COLOR_RED);
+    mTrial.SetPixelColor(12, COLOR_PURPLE);
+    mTrial.SetPixelColor(13, COLOR_GREEN);
+    mTrial.SetPixelColor(14, COLOR_PURPLE);
+    mTrial.SetPixelColor(15, COLOR_RED);
+    mTrial.SetPixelColor(16, COLOR_PURPLE);
+    mTrial.SetPixelColor(17, COLOR_GREEN);
+    mTrial.SetPixelColor(18, COLOR_PURPLE);
+    mTrial.SetPixelColor(19, COLOR_RED);
+    mTrial.SetPixelColor(20, COLOR_PURPLE);
+    //processBlendedAnimationForever(0, PIXEL_COUNT, ONE_SECOND_DURATION, 3, RGBTestColors);
+    mTrial.Show();
+#endif
+
 
     return 0;
 }
--- src/NeoPixelBus.h
+++ src/NeoPixelBus.h
@@ -73,6 +73,8 @@ License along with NeoPixel.  If not, see
 #include "internal/Ws2801GenericMethod.h"
 #include "internal/P9813GenericMethod.h"
 
+#include <QDebug>
+
 
 
 
@@ -87,6 +89,7 @@ public:
         _state(0),
         _method(pin, countPixels, T_COLOR_FEATURE::PixelSize, T_COLOR_FEATURE::SettingsSize)
     {
+        qDebug()<<__LINE__;
     }
 
     NeoPixelBus(uint16_t countPixels, uint8_t pinClock, uint8_t pinData) :
@@ -94,6 +97,7 @@ public:
         _state(0),
         _method(pinClock, pinData, countPixels, T_COLOR_FEATURE::PixelSize, T_COLOR_FEATURE::SettingsSize)
     {
+        qDebug()<<__LINE__;
     }
 
     NeoPixelBus(uint16_t countPixels) :
@@ -101,6 +105,7 @@ public:
         _state(0),
         _method(countPixels, T_COLOR_FEATURE::PixelSize, T_COLOR_FEATURE::SettingsSize)
     {
+        qDebug()<<__LINE__;
     }
 
     ~NeoPixelBus()
@@ -115,13 +120,17 @@ public:
 
     void Begin()
     {
+    //ue
+        qDebug()<<__LINE__;
         _method.Initialize();
+        //ClearTo(0);
         Dirty();
     }
 
     // used by DotStartSpiMethod if pins can be configured
     void Begin(int8_t sck, int8_t miso, int8_t mosi, int8_t ss)
     {
+        qDebug()<<__LINE__;
         _method.Initialize(sck, miso, mosi, ss);
         Dirty();
     }
